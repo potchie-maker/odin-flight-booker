748380a7580a78b54415d9a46291a5a1966d082b
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2026-04-09 16:38:18 -0400

    Setup FlightsController, routes, and change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :flights, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "flights#index"
 end
