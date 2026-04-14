2c372fc9c68b96c3436c84519bcb28831f57a3a2
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2026-04-14 01:15:05 -0400

    Finish simple search functionality

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,11 +2,25 @@ class FlightsController < ApplicationController
   def index
     @departure_options = Flight.all.map{ |f| f.departure_airport.airport_code }.uniq
     @arrival_options = Flight.all.map{ |f| f.arrival_airport.airport_code }.uniq
-    @date_options = Flight.all.map{ |f| [f.start_datetime_formatted, f.start_datetime] }.uniq
+    @date_options = Flight.all.order(:start_datetime).map{ |f| [f.start_datetime_formatted, f.start_datetime.iso8601(9)] }.uniq
+    # To find a method to access the full timestamp, otherwise it doesn't show the subseconds. Since the random
+    # dates from the seeds have subseconds, the queries weren't matching or returning any records.
+    # Google gemini suggested to use #iso8601, and then specifying the decimal precision,
+    # i.e. start_datetime.iso8601(9). It said #inspect is only for debugging (but works technically).
+    # Also found you can use #round(n), and Time.at(datetime.to_r).
+    # Can also use #strftime("%Y-%m-%d %H:%M:%S.%N") (year has to be first to match db formatting).
+    #
+    # Maybe adding and accessing via index is the way to go? Like using the index as the selected option instead of the
+    # literal date. Seems closer to saving an :id and more straightforwared.
 
     search_params = flight_search_params
+    # assigned search params to a variable because it makes the method only be called once
+    # so I THINK that it might offer slightly more efficiency/speed
+    # otherwise, I can just access directly from params as I wrote over the private method
     unless search_params.empty?
-      @searched_flights = Flight.where(departure_airport: Airport.find_by(airport_code: search_params[:departure_airport]))
+      @searched_flights = Flight.get_flights_from_search(search_params[:departure_airport],
+                                                         search_params[:arrival_airport],
+                                                         search_params[:start_datetime])
     end
   end
 
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,7 +7,25 @@ class Flight < ApplicationRecord
   validates :start_datetime, presence: true
   validates :flight_duration, presence: true
 
-  # Source - https://stackoverflow.com/a/15721423
+
+  def self.get_flights_from_search(dep_ap_code, arr_ap_code, start_date)
+    # Flight.where(departure_airport: Airport.find_by(airport_code: dep_ap_code))
+    #   .or(Flight.where(arrival_airport: Airport.find_by(airport_code: arr_ap_code)))
+    #   .or(Flight.where(start_datetime: start_date)).order("departure_airport_id", "arrival_airport_id")
+
+    # Flight.where("departure_airport_id = ? OR arrival_airport_id = ? OR start_datetime = ?",
+    #              Airport.find_by(airport_code: dep_ap_code), Airport.find_by(airport_code: arr_ap_code), start_date)
+
+    search_results = {}
+    # this method of saving the options in a hash allows me to order the results based on more important criteria
+    search_results[:by_dep_airport] = Flight.where(departure_airport: Airport.find_by(airport_code: dep_ap_code))
+    search_results[:by_arr_airport] = Flight.where(arrival_airport: Airport.find_by(airport_code: arr_ap_code))
+    search_results[:by_start_date] = Flight.where(start_datetime: start_date)
+
+    return search_results
+  end
+
+# Source - https://stackoverflow.com/a/15721423
 # Posted by Luís Ramalho
 # Retrieved 2026-04-09, License - CC BY-SA 3.0
 
